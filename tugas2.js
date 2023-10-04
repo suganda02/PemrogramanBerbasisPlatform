c9c61acdbf725600bd21addea8deef0cabab7cb5
Suganda <129074317+suganda02@users.noreply.github.com>
2023-10-04 21:28:15 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,9 @@
+var firstName = "SUGANDA";
+var lastName = "GANDA";
+var age = 21; 
+var isMarried = 27;
+
+console.log (firstName);
+console.log (lastName);
+console.log (age);
+console.log (isMarried);
